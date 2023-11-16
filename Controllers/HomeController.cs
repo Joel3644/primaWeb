d4b6d6a099c40f6b4b60f09e891cc41ac0f41970
JoelDervishi <joel.dervishi@studenti.ittsrimini.edu.it>
2023-11-16 20:35:24 +0100

    Project Upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using dervishi.joel._5i.PrimaWeb.Models;
+
+namespace dervishi.joel._5i.PrimaWeb.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Prodotto()
+    {
+        return View();
+    }
+    
+    [HttpGet]
+    public IActionResult Prenota()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Prenota(Prenotazione p)
+    {
+        return View(p);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
